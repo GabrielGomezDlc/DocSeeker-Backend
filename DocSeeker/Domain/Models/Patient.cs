c5b630214c1f13ebad6fe0725bb729d483acfe3b
GabrielGomezDlc <u202020341@upc.edu.pe>
2022-11-23 00:42:28 -0500

    Initial Commit

---
+++ DocSeeker/Domain/Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿using DocSeeker.API.Security.Domain.Models;
+
+namespace DocSeeker.API.DocSeeker.Domain.Models
+{
+    public class Patient : User
+    {
+        //public int Id { get; set; }
+
+        public IList<Date> CDates { get; set; } = new List<Date>();
+        public IList<Review> CReviews { get; set; } = new List<Review>();
+        public IList<MedicalInformation> CMedicalInformation { get; set; } = new List<MedicalInformation>();
+        public IList<Prescription> CPrescription { get; set; } = new List<Prescription>();
+
+    }
+}
